e4a7ba9874d6be8dbf45ab05e3f4a77191b24b0e
Gokul Shinde <shindegokul7841@gmail.com>
2026-01-23 18:00:23 +0000

    make the borders 5px curve from every corner of every box/button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,8 +75,8 @@ export default {
       },
       borderRadius: {
         lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 4px)',
-        sm: 'calc(var(--radius) - 8px)',
+        md: 'var(--radius)',
+        sm: 'var(--radius)',
       },
       boxShadow: {
         soft: '0 4px 14px 0 hsla(0, 0%, 0%, 0.1)',
