46a68c0f0f0e513e0370537f21711adc1a73633b
Gokul Shinde <shindegokul7841@gmail.com>
2026-01-23 17:41:04 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -98,8 +98,8 @@ export default async function AboutPage() {
               </CardContent>
             </Card>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   );
 }
