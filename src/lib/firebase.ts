d1066abf3f6379ce10accde46664943ca7c02b33
Gokul Shinde <shindegokul7841@gmail.com>
2026-01-23 12:26:24 +0000

    I have created the firebase project.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,22 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// IMPORTANT: Replace this with your own Firebase configuration
-// and store it in environment variables.
-// Example for .env.local
-// NEXT_PUBLIC_FIREBASE_API_KEY="your-api-key"
-// NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="your-auth-domain"
-// NEXT_PUBLIC_FIREBASE_PROJECT_ID="your-project-id"
-// NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="your-storage-bucket"
-// NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="your-messaging-sender-id"
-// NEXT_PUBLIC_FIREBASE_APP_ID="your-app-id"
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyC5...<redacted>",
+    authDomain: "devfolio-f3a74.firebaseapp.com",
+    projectId: "devfolio-f3a74",
+    storageBucket: "devfolio-f3a74.appspot.com",
+    messagingSenderId: "100...<redacted>",
+    appId: "1:100...<redacted>",
+    measurementId: "G-C...<redacted>"
 };
 
 // Initialize Firebase
